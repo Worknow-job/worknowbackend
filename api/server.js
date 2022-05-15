17358193e6b7f99ffc06569fce1067bddaf17392
Tgleason1980 <terryscoding@gmail.com>
2022-05-14 23:50:37 -0500

    added workers

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const server = express()
 
 const usersRouter = require('../routes/users/users-router')
 // const driverRouter = require('../routes/drivers/driver-router')
-const workerssRouter = require('../routes/workers/workers-router')
+const workersRouter = require('../routes/workers/workers-router')
 const authRouther = require('../routes/auth/auth-router')
 
 server.use(express.json(), helmet(), cors())
